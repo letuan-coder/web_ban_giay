a9c7b6708433d6c5d72bae2a8c34d949a2c6ed66
Tuấn Lê <letuan28092002@gmail.com>
2025-11-22 16:07:49 +0700

    file spectification

---
+++ DATN/src/main/java/com/example/DATN/specification/ProductSpecification.java
@@ -0,0 +1,38 @@
+package com.example.DATN.specification;
+
+import com.example.DATN.constant.ProductStatus;
+import com.example.DATN.models.Product;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductSpecification {
+
+    public static Specification<Product> filterProducts(
+            String productName, Double priceMin, Double priceMax, ProductStatus status) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (productName != null && !productName.isEmpty()) {
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + productName.toLowerCase() + "%"));
+            }
+
+            if (priceMin != null) {
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("price"), new BigDecimal(priceMin)));
+            }
+
+            if (priceMax != null) {
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("price"), new BigDecimal(priceMax)));
+            }
+
+            if (status != null) {
+                predicates.add(criteriaBuilder.equal(root.get("available"), status));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+}
